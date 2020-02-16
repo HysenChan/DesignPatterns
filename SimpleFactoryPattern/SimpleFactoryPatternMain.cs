d6fed979b5692fb8db3a767be53e40a22fb48240
HysenChan <17602059496@163.com>
2020-02-16 15:47:55 +0800

    简单工厂模式-无扩展性代码

--- SimpleFactoryPattern/SimpleFactoryPatternMain.cs
+++ SimpleFactoryPattern/SimpleFactoryPatternMain.cs
@@ -10,8 +10,25 @@ namespace SimpleFactoryPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Simple Factory Pattern!");
-            Console.ReadLine();
+            Console.WriteLine("请输入数字A：");
+            string A = Console.ReadLine();
+            Console.WriteLine("请输入运算符号(+、-、*、/)：");
+            string B = Console.ReadLine();
+            Console.WriteLine("请输入数字B：");
+            string C = Console.ReadLine();
+            string D = "";
+
+            if (B=="+")
+                D = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(C));
+            if (B == "-")
+                D = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(C));
+            if (B == "*")
+                D = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(C));
+            if (B == "/")
+                D = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(C));
+
+            Console.WriteLine("结果是："+D);
+            Console.ReadKey();
         }
     }
 }

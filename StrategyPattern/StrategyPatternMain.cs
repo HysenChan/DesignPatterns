3c818db36660497b070445412c84a097b29839f1
HysenChan <17602059496@163.com>
2020-02-17 17:30:12 +0800

    策略模式-初始化项目

---
+++ StrategyPattern/StrategyPatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class StrategyPatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is StrategyPatternMain!");
+            Console.ReadLine();
+        }
+    }
+}

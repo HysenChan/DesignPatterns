3333cf7842caaeb41a84597f7e0cb0cf3b7660f4
HysenChan <17602059496@163.com>
2020-02-17 21:30:59 +0800

    策略模式-利用简单工厂模式实现

--- StrategyPattern/StrategyPatternMain.cs
+++ StrategyPattern/StrategyPatternMain.cs
@@ -10,7 +10,12 @@ namespace StrategyPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is StrategyPatternMain!");
+            CashSuper cashSuper = CashFactory.createCashAccept("满300减100");
+            double totalPrices = 0d;
+            double total=0d,price = 130d, num = 3d;
+            totalPrices = cashSuper.acceptCash(price * num);
+            total += totalPrices;
+            Console.WriteLine("单价为：{0}，数量：{1}，总价：{2}",price,num,total);
             Console.ReadLine();
         }
     }

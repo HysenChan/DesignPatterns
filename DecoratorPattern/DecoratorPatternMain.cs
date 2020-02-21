c4a1626dc4f0f88365742ad0dc7e27169cf6451e
HysenChan <17602059496@163.com>
2020-02-21 16:29:30 +0800

    装饰模式-完整实现动态添加更多功能

--- DecoratorPattern/DecoratorPatternMain.cs
+++ DecoratorPattern/DecoratorPatternMain.cs
@@ -8,7 +8,7 @@ namespace DecoratorPattern
 {
     class DecoratorPatternMain
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             ConcreteComponent c = new ConcreteComponent();
             ConcreteDecoratorA d1 = new ConcreteDecoratorA();
@@ -17,6 +17,39 @@ namespace DecoratorPattern
             d1.SetComponent(c);
             d2.SetComponent(d1);
             d2.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
+
+namespace DecoratorPatternPerson
+{
+    class DecoratorPatternPerson
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Hysen");
+            Console.WriteLine("First dress up:");
+
+            TShirts shirts = new TShirts();
+            BitTrouser bitTrouser = new BitTrouser();
+            Tie tie = new Tie();
+            LeatherShoes leatherShoes = new LeatherShoes();
+
+            //大T恤 垮裤 领带 皮鞋 装扮的Hysen
+            shirts.Decorate(bitTrouser);
+            bitTrouser.Decorate(tie);
+            tie.Decorate(leatherShoes);
+            leatherShoes.Decorate(person);
+            shirts.Show();
+
+            //皮鞋 领带 垮裤 大T恤 装扮的Hysen
+            //shirts.Decorate(person);
+            //bitTrouser.Decorate(shirts);
+            //tie.Decorate(bitTrouser);
+            //leatherShoes.Decorate(tie);
+            //leatherShoes.Show();
 
             Console.ReadKey();
         }
---
+++ DecoratorPattern/Derived/Finery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPatternPerson
+{
+    class Finery:Person
+    {
+        protected Person person;
+
+        //打扮
+        public void Decorate(Person person)
+        {
+            this.person = person;
+        }
+
+        public override void Show()
+        {
+            if (person!=null)
+            {
+                person.Show();
+            }
+        }
+    }
+}

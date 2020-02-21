e6c0663bc3a0a1a5ae7c64db5be76118df66c5ed
HysenChan <17602059496@163.com>
2020-02-21 15:28:03 +0800

    装饰器模式-把所需功能按正确顺序串联起来进行控制

--- DecoratorPattern/DecoratorPatternMain.cs
+++ DecoratorPattern/DecoratorPatternMain.cs
@@ -10,7 +10,14 @@ namespace DecoratorPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("这是装饰模式！");
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+            d2.Operation();
+
             Console.ReadKey();
         }
     }

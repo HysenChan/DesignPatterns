7979cf8bb4d41d8d6edacedd73e325f241461161
HysenChan <17602059496@163.com>
2020-02-21 14:53:48 +0800

    装饰模式-初始化项目

---
+++ DecoratorPattern/DecoratorPatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    class DecoratorPatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是装饰模式！");
+            Console.ReadKey();
+        }
+    }
+}

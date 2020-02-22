a4af40da16290a5158a53981e6e1d25fd6a56439
HysenChan <17602059496@163.com>
2020-02-22 10:15:34 +0800

    代理模式-初始化项目

---
+++ ProxyPattern/ProxyPatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class ProxyPatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是代理模式！");
+            Console.ReadKey();
+        }
+    }
+}

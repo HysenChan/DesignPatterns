2da386ca434144f71c38725cc9fd115652635646
HysenChan <17602059496@163.com>
2020-02-23 10:59:44 +0800

    工厂方法模式-简单工厂类实现

--- FactoryMethodPattern/FactoryMethodPatternMain.cs
+++ FactoryMethodPattern/FactoryMethodPatternMain.cs
@@ -10,10 +10,14 @@ namespace FactoryMethodPattern
     {
         static void Main(string[] args)
         {
-            LeiFeng leiFeng = new Undergraduate();
-            leiFeng.BuyRice();
-            leiFeng.Sweep();
-            leiFeng.Wash();
+            //TODO:简单工厂的实例化时会有重复代码，后面会用工厂方法类
+            LeiFeng v1 = SimpleFactory.CreateLeiFeng("社区志愿者");
+            LeiFeng v2 = SimpleFactory.CreateLeiFeng("社区志愿者");
+            LeiFeng s1 = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+
+            v1.Wash();
+            v2.Sweep();
+            s1.BuyRice();
 
             Console.ReadKey();
         }

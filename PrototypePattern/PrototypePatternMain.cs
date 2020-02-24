74626fd4f99b3c46cbf0d59d49824235eaf82550
HysenChan <17602059496@163.com>
2020-02-24 10:39:36 +0800

    原型模式-初始化项目

---
+++ PrototypePattern/PrototypePatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class PrototypePatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是原型模式！");
+            Console.ReadKey();
+        }
+    }
+}

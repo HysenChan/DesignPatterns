a1d7feea0fff84805fbbc467a4209d0b29bd9279
HysenChan <17602059496@163.com>
2020-02-25 11:57:42 +0800

    将子类变量的声明改成父类，利用了多态性

--- TemplateMethodPattern/TemplateMethodPatternMain.cs
+++ TemplateMethodPattern/TemplateMethodPatternMain.cs
@@ -11,13 +11,13 @@ namespace TemplateMethodPattern
         static void Main(string[] args)
         {
             Console.WriteLine("甲抄的试卷：");
-            TestPaperA testPaperA = new TestPaperA();
+            TestPaper testPaperA = new TestPaperA();
             testPaperA.TestQusetion1();
             testPaperA.TestQusetion2();
             testPaperA.TestQusetion3();
 
             Console.WriteLine("乙抄的试卷：");
-            TestPaperB testPaperB = new TestPaperB();
+            TestPaper testPaperB = new TestPaperB();
             testPaperB.TestQusetion1();
             testPaperB.TestQusetion2();
             testPaperB.TestQusetion3();

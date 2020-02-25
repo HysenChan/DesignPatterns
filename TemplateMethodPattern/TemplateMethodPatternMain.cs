ff8d4924e673ea43769ec3cf413279988e479692
HysenChan <17602059496@163.com>
2020-02-25 11:27:26 +0800

    模板方法模式-未使用模板方法模式的实现

--- TemplateMethodPattern/TemplateMethodPatternMain.cs
+++ TemplateMethodPattern/TemplateMethodPatternMain.cs
@@ -10,7 +10,17 @@ namespace TemplateMethodPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("这是模板方法模式！");
+            Console.WriteLine("甲抄的试卷：");
+            TestPaperA testPaperA = new TestPaperA();
+            testPaperA.TestQusetion1();
+            testPaperA.TestQusetion2();
+            testPaperA.TestQusetion3();
+
+            Console.WriteLine("乙抄的试卷：");
+            TestPaperB testPaperB = new TestPaperB();
+            testPaperB.TestQusetion1();
+            testPaperB.TestQusetion2();
+            testPaperB.TestQusetion3();
             Console.ReadKey();
         }
     }

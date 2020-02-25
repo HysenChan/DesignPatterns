5f38a19f75d89f9805ffc6024d22fa2451883866
HysenChan <17602059496@163.com>
2020-02-25 11:42:54 +0800

    模板方法模式-提炼代码

---
+++ TemplateMethodPattern/TestPaper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    class TestPaper
+    {
+        public void TestQusetion1()
+        {
+            Console.WriteLine("杨过得到，后来给了郭靖，炼成倚天剑、屠龙刀的玄铁可能是【】a.球墨铸铁 b.马口铁 c.高速合金钢 d.碳素纤维");
+        }
+
+        public void TestQusetion2()
+        {
+            Console.WriteLine("杨过、程英、陆无双铲除了情花，造成【】a.使这种植物不再害人 b.使一种珍稀物种灭绝 c.破坏力生态平衡 d.造成沙漠化");
+        }
+
+        public void TestQusetion3()
+        {
+            Console.WriteLine("蓝凤凰致使华山师徒、桃谷六仙呕吐不止，应该吃什么药【】a.阿司匹林 b.牛黄解毒片 c.999小柴胡 d.喝大量的牛奶");
+        }
+    }
+}
--- TemplateMethodPattern/TestPaperA.cs
+++ TemplateMethodPattern/TestPaperA.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace TemplateMethodPattern
 {
-    class TestPaperA
+    class TestPaperA:TestPaper
     {
-        public void TestQusetion1()
+        public new void TestQusetion1()
         {
-            Console.WriteLine("杨过得到，后来给了郭靖，炼成倚天剑、屠龙刀的玄铁可能是【】a.球墨铸铁 b.马口铁 c.高速合金钢 d.碳素纤维");
-            Console.WriteLine("b");
-        }
-
-        public void TestQusetion2()
-        {
-            Console.WriteLine("杨过、程英、陆无双铲除了情花，造成【】a.使这种植物不再害人 b.使一种珍稀物种灭绝 c.破坏力生态平衡 d.造成沙漠化");
+            base.TestQusetion1();
             Console.WriteLine("a");
         }
 
-        public void TestQusetion3()
+        public new void TestQusetion2()
         {
-            Console.WriteLine("蓝凤凰致使华山师徒、桃谷六仙呕吐不止，应该吃什么药【】a.阿司匹林 b.牛黄解毒片 c.999小柴胡 d.喝大量的牛奶");
+            base.TestQusetion2();
             Console.WriteLine("b");
+        }
+
+        public new void TestQusetion3()
+        {
+            base.TestQusetion3();
+            Console.WriteLine("c");
         }
     }
 }

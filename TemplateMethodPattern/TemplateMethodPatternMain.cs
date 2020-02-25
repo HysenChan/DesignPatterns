9ed6f790a490a376aa82e33a594edde5a355df50
HysenChan <17602059496@163.com>
2020-02-25 11:02:28 +0800

    模板方法模式-初始化项目

---
+++ TemplateMethodPattern/TemplateMethodPatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    class TemplateMethodPatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是模板方法模式！");
+            Console.ReadKey();
+        }
+    }
+}

43b2b841abde9fbae4681c9b89da2c89c645020d
HysenChan <17602059496@163.com>
2020-02-27 14:17:14 +0800

    外观模式-初始化项目

---
+++ FacadePattern/FacadePatternMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    class FacadePatternMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是外观模式！");
+            Console.ReadKey();
+        }
+    }
+}
